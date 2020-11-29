5f7ec12958a379077f0b486a4fcb1dee6c0c4f0c
krystian.krasowski <krystiankrasowski@gmail.com>
2020-11-29 11:41:17 +0100

    extracted abstract factory for user registration result

---
+++ application/src/main/java/com/smtm/application/users/endpoint/v1/UserRegistrationResponseFactory.java
@@ -0,0 +1,70 @@
+package com.smtm.application.users.endpoint.v1;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.ResponseEntity;
+import com.smtm.application.validation.v1.ConstraintViolationsDto;
+import com.smtm.application.validation.v1.ValidationMediaTypes;
+import com.smtm.users.registration.UserProfile;
+
+abstract class UserRegistrationResponseFactory<T extends UserProfile> {
+
+    T userProfile;
+
+    UserRegistrationResponseFactory(T userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    static <T extends UserProfile> UserRegistrationResponseFactory<?> getFactoryFor(T userProfile) {
+        if (userProfile instanceof UserProfile.Valid) {
+            return new UserProfileDtoFactory((UserProfile.Valid) userProfile);
+        }
+
+        if (userProfile instanceof UserProfile.Invalid) {
+            return new ConstraintsViolationsDtoFactory((UserProfile.Invalid) userProfile);
+        }
+
+        throw new IllegalArgumentException("Unsupported result type");
+    }
+
+    abstract ResponseEntity<?> create();
+
+    static class UserProfileDtoFactory extends UserRegistrationResponseFactory<UserProfile.Valid> {
+
+        UserProfileDtoFactory(UserProfile.Valid userProfile) {
+            super(userProfile);
+        }
+
+        @Override
+        public ResponseEntity<?> create() {
+            UserProfileDto user = UserProfileDto.of(userProfile);
+            EntityModel<UserProfileDto> model = createUserProfileRepresentation(user);
+            return ResponseEntity
+                .created(linkTo(methodOn(UsersController.class).getUser(user.getId())).toUri())
+                .contentType(UserMediaTypes.USER_PROFILE)
+                .body(model);
+        }
+
+        private EntityModel<UserProfileDto> createUserProfileRepresentation(UserProfileDto user) {
+            return EntityModel.of(user)
+                .add(linkTo(methodOn(UsersController.class).getUser(user.getId())).withSelfRel());
+        }
+    }
+
+    static class ConstraintsViolationsDtoFactory extends UserRegistrationResponseFactory<UserProfile.Invalid> {
+
+        ConstraintsViolationsDtoFactory(UserProfile.Invalid userProfile) {
+            super(userProfile);
+        }
+
+        @Override
+        public ResponseEntity<?> create() {
+            ConstraintViolationsDto violationsDto = ConstraintViolationsDto.of(userProfile);
+            return ResponseEntity
+                .badRequest()
+                .contentType(ValidationMediaTypes.CONSTRAINT_VIOLATION)
+                .body(violationsDto);
+        }
+    }
+}
--- application/src/main/java/com/smtm/application/users/endpoint/v1/UsersController.java
+++ application/src/main/java/com/smtm/application/users/endpoint/v1/UsersController.java
@@ -1,15 +1,11 @@
 package com.smtm.application.users.endpoint.v1;
 
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
-import org.jetbrains.annotations.NotNull;
+import java.util.Optional;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.smtm.application.validation.v1.ConstraintViolationsDto;
 import com.smtm.application.validation.v1.ValidationMediaTypes;
 import com.smtm.users.api.UserRegistration;
-import com.smtm.users.registration.UserProfile;
 
 @RestController
 public class UsersController {
@@ -38,43 +34,9 @@ public class UsersController {
         }
     )
     public ResponseEntity<?> registerUser(@RequestBody NewUserDto user) {
-        UserProfile userProfile = userRegistration.register(user.getEmail(), user.getPasswordAsUnsecured());
-        return createResponse(userProfile);
-    }
-
-    private ResponseEntity<?> createResponse(UserProfile userProfile) {
-        if (userProfile instanceof UserProfile.Valid) {
-            return createUserProfileResponse((UserProfile.Valid) userProfile);
-        }
-
-        if (userProfile instanceof UserProfile.Invalid) {
-            return createConstraintViolationsResponse((UserProfile.Invalid) userProfile);
-        }
-
-        throw new IllegalArgumentException("Unsupported result type");
-    }
-
-    @NotNull
-    private ResponseEntity<EntityModel<UserProfileDto>> createUserProfileResponse(UserProfile.Valid userProfile) {
-        UserProfileDto user = UserProfileDto.of(userProfile);
-        EntityModel<UserProfileDto> model = createUserProfileRepresentation(user);
-        return ResponseEntity
-            .created(linkTo(methodOn(UsersController.class).getUser(user.getId())).toUri())
-            .contentType(UserMediaTypes.USER_PROFILE)
-            .body(model);
-    }
-
-    @NotNull
-    private ResponseEntity<ConstraintViolationsDto> createConstraintViolationsResponse(UserProfile.Invalid userProfile) {
-        ConstraintViolationsDto violationsDto = ConstraintViolationsDto.of(userProfile);
-        return ResponseEntity
-            .badRequest()
-            .contentType(ValidationMediaTypes.CONSTRAINT_VIOLATION)
-            .body(violationsDto);
-    }
-
-    private EntityModel<UserProfileDto> createUserProfileRepresentation(UserProfileDto user) {
-        return EntityModel.of(user)
-            .add(linkTo(methodOn(UsersController.class).getUser(user.getId())).withSelfRel());
+        return Optional.of(userRegistration.register(user.getEmail(), user.getPasswordAsUnsecured()))
+            .map(UserRegistrationResponseFactory::getFactoryFor)
+            .map(UserRegistrationResponseFactory::create)
+            .orElseThrow(() -> new IllegalStateException("Cannot register user"));
     }
 }
