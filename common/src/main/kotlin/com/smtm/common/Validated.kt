7b88311854603b2924beccbb4da2ed4c4b527d31
krystian.krasowski <krystiankrasowski@gmail.com>
2021-03-15 18:31:05 +0100

    created Validated monad

---
+++ common/src/main/kotlin/com/smtm/common/Validated.kt
@@ -0,0 +1,18 @@
+package com.smtm.common
+
+sealed class Validated<T>(private val subject: T?, private val violations: List<ConstraintViolation>) {
+
+    class Success<T> internal constructor(subject: T) : Validated<T>(subject, emptyList())
+    class Failure<T> internal constructor(violations: List<ConstraintViolation>) : Validated<T>(null, violations)
+
+    fun <R> map(transform: (T) -> R): Validated<R> = subject
+        ?.let(transform)
+        ?.let { validationSuccessOf(it) }
+        ?: this as Validated<R>
+
+    fun orElse(transform: (List<ConstraintViolation>) -> T) = subject ?: transform(violations)
+}
+
+fun <T> validationSuccessOf(subject: T): Validated<T> = Validated.Success(subject)
+
+fun validationFailureOf(violations: List<ConstraintViolation>): Validated<Nothing> = Validated.Failure(violations)
