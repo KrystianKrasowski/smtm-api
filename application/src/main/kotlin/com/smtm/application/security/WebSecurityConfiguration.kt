1f2fdd2331e0b7048193cc3d0e50f5e1e3c0674c
krystian.krasowski <krystiankrasowski@gmail.com>
2021-07-11 12:30:24 +0200

    added development profile and cors processing

--- application/src/main/kotlin/com/smtm/application/security/WebSecurityConfiguration.kt
+++ application/src/main/kotlin/com/smtm/application/security/WebSecurityConfiguration.kt
@@ -1,12 +1,17 @@
 package com.smtm.application.security
 
 import com.smtm.security.api.Authorization
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @EnableWebSecurity
 class WebSecurityConfiguration(private val authorization: Authorization) : WebSecurityConfigurerAdapter() {
@@ -18,6 +23,7 @@ class WebSecurityConfiguration(private val authorization: Authorization) : WebSe
     }
 
     override fun configure(http: HttpSecurity) {
+        http.cors()
         http.csrf()
             .disable()
 
@@ -28,5 +34,16 @@ class WebSecurityConfiguration(private val authorization: Authorization) : WebSe
             .addFilter(JwtAuthorizationFilter(authenticationManager(), authorization))
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    }
+}
+
+@Configuration
+@EnableWebMvc
+@Profile("development")
+class DevelopmentWebMvcConfiguration: WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry
+            .addMapping("/**")
     }
 }
